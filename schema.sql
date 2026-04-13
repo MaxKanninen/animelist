84f6c4d187fb8c2332a63f2247cc71cfd2b31c78
MaxKanninen <kanninen.max@gmail.com>
2026-04-13 12:07:33 +0300

    Add NOT NULL constraints to schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    password_hash TEXT
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
 );
 
 CREATE TABLE series (
     id INTEGER PRIMARY KEY,
-    title TEXT,
-    description TEXT,
-    year INTEGER,
-    episodes INTEGER,
-    user_id INTEGER REFERENCES users,
-    created_at TEXT
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    episodes INTEGER NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users,
+    created_at TEXT NOT NULL
 );
